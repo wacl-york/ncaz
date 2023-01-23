de750a30cb011b973cf6855c2c63854228031cf4
Stuart Lacy <stuart.lacy@york.ac.uk>
2023-01-23 15:16:09 +0000

    Added supporting text

--- app.R
+++ app.R
@@ -1,12 +1,63 @@
 library(shiny)
+library(shinycssloaders)
 library(tidyverse)
 library(lubridate)
 library(plotly)
 source("utils.R")
 
-ui <- fluidPage(
-  titlePanel("Modelling the impact of the Newcastle and Gateshead Clean Air Zone"),
-  mainPanel(plotlyOutput("distPlot"))
+ui <- navbarPage("Newcastle and Gateshead CAZ",
+                 tabPanel("Live monitor",
+                          sidebarLayout(
+                            sidebarPanel(
+                              h3("Measuring the impact of the Newcastle and Gateshead Clean Air Zone"),
+                              h4("Newcastle & Gateshead CAZ"),
+                              HTML("A Clean Air Zone (CAZ) is being introduced to Newcastle and Gateshead in an attempt to improve air quality."),
+                              HTML("The zone covers a large portion of central Newcastle as well as roads into Gateshead."),
+                              HTML("Initially penalties only apply to taxis, buses, coaches, and HGVs from 30th January 2023, with vans and LGVs being affected in July 2023."),
+                              HTML("See the official website <a href='https://www.breathe-cleanair.com/'>for further details</a>."),
+                              br(),
+                              h4("Detrending NO2"),
+                              HTML("This page attempts to quantify the impact of the CAZ on reducing emissions of NO2."),
+                              HTML("Unfortunately, this isn't as straightforward as simply measuring NO2 directly, as NO2 concentrations are the result of complex physical relationships between both natural and man-made systems."),
+                              HTML("Notably, average NO2 levels vary throughout the year, and are further impacted by local meteorological conditions."),
+                              HTML("The <span style='color:#1F77B4'>blue</span> lines show raw daily average NO2 measurements from 2 recording sites that are part of DEFRA's Automatic Urban and Rural Network (AURN):"),
+                              HTML("<a href='https://uk-air.defra.gov.uk/networks/site-info?site_id=NEWC'>Newcastle Centre</a> and "),
+                              HTML("<a href='https://uk-air.defra.gov.uk/networks/site-info?site_id=NCA3'>Newcastle Cradlewell Roadside</a>, and display considerable short-term variance."),
+                              
+                              br(),
+                              h4("Statistical modelling"),
+                              HTML("To quantify the impact of the CAZ on reducing NO2 concentrations, a statistical model has been developed to remove the effects of seasonality and weather on measured NO2."),
+                              HTML("The resulting values are shown in the <span style='color:#FF7F04'>orange</span> lines, which are much smoother."),
+                              HTML("If the CAZ does result in reduced NO2 emissions, it should be evident here."),
+                              HTML("See the Methodology tab for further details."),
+                              br(),
+                              br(),
+                              HTML("<span style='color:#FF0000'>Disclaimer: this is a non-validated piece of research work that should not be referenced in its current state.</span>")
+                            ),
+                            mainPanel(
+                              withSpinner(plotlyOutput("detrended"))
+                            )
+                          )
+                 ),
+                 tabPanel("Methodology",
+                          HTML("This page contains a description of the state-space model (SSM) that has been used for the meteorological and seasonal detrending."),
+                          HTML("A SSM (sometimes referred to as a Kalman-Filter) is a statistical model of a time-series that defines the observed measurements as"),
+                          HTML("a linear function of one or more unobserved <emph>states</emph>, along with a fully specified model of the state's dynamics."),
+                          br(),
+                          br(),
+                          withMathJax(HTML("Observation equation: $$y_t = \\alpha_t + \\lambda_t + \\beta X_t + \\epsilon_t$$")),
+                          HTML("State equation: $$\\alpha_t = \\alpha_{t-1} + \\eta_t$$"),
+                          HTML("Where: "),
+                          HTML("<ul>"),
+                          HTML("<li>\\(y_t\\) = measured NO2</li>"),
+                          HTML("<li>\\(\\alpha_t\\) = underlying NO2 detrended (state)</li>"),
+                          HTML("<li>\\(\\lambda_t\\) = seasonal factors (state)</li>"),
+                          HTML("<li>\\(X_t\\) = meteorological measurements</li>"),
+                          HTML("<li>\\(\\beta\\) = coefficients for meteorological factors (state)</li>"),
+                          HTML("<li>\\(\\epsilon_t\\) = variance unexplained by the model</li>"),
+                          HTML("<li>\\(\\eta_t\\) = how much the NO2 detrend can update each time-step (on a random walk)</li>"),
+                          HTML("</ul>")
+                 )
 )
 
 server <- function(input, output) {
@@ -24,7 +75,7 @@ server <- function(input, output) {
     )
   
   
-  output$distPlot <- renderPlotly({
+  output$detrended <- renderPlotly({
     p1 <- plot_ly(df %>% filter(code == 'NEWC'), x =  ~ time) %>%
       add_lines(y =  ~ no2,
                 name = "Measured",
@@ -38,7 +89,7 @@ server <- function(input, output) {
                 name = "Business-as-usual",
                 color = I("#2CA02C")) %>%
       layout(xaxis = list(range = c(
-        as_datetime("2022-12-01"), max(df$time)
+        today() - months(3), today()
       )),
       yaxis = list(title = "NO2 (ppb)")) %>%
       add_annotations(
@@ -58,7 +109,7 @@ server <- function(input, output) {
         y =  ~ no2,
         name = "Measured",
         color = I("#1F77B4"),
-        showlegend = FALSE,
+        showlegend = FALSE
       ) %>%
       add_lines(
         y =  ~ detrended,
@@ -73,7 +124,7 @@ server <- function(input, output) {
         showlegend = FALSE
       ) %>%
       layout(xaxis = list(range = c(
-        as_datetime("2022-12-01"), max(df$time)
+        today() - months(3), today()
       )),
       yaxis = list(title = "NO2 (ppb)")) %>%
       add_annotations(
